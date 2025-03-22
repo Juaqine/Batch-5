968b81cecd3e674c11d88f79e5a5905488ac09ef
Juaqine <juaqineynicoavila@gmail.com>
2025-03-22 12:40:06 +0800

    Update Batch5Prog06.py

--- Batch5Prog06.py
+++ Batch5Prog06.py
@@ -1,4 +1,4 @@
-Prog06: Create a program that ask the user to input their fullname in incorrect casing. Print each character of the input in reverse casing.
-Example:
-Input: jUAn DEla CrUZ
-Output: JuaN deLA cRuz
+#Prog06: Create a program that ask the user to input their fullname in incorrect casing. Print each character of the input in reverse casing.
+#Example:
+#Input: jUAn DEla CrUZ
+#Output: JuaN deLA cRuz
